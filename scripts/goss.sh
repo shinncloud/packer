c89e57b2104721cd08f710fbfc9a2cb56c799bc3
Andy Shinn <315485+andyshinn@users.noreply.github.com>
2019-01-12 19:50:34 -0800

    goss

---
+++ scripts/goss.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+GOSS_VERSION="${1:-v0.3.6}"
+
+curl -L https://github.com/aelsabbahy/goss/releases/download/$GOSS_VERSION/goss-linux-amd64 -o /usr/local/bin/goss
+chmod +rx /usr/local/bin/goss
+systemctl daemon-reload
+systemctl enable goss
