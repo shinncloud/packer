95c7cc55f0cecf7cfcf331fb32eb7cc25635f861
Andy Shinn <315485+andyshinn@users.noreply.github.com>
2019-06-15 21:02:38 -0500

    bump goss version

--- scripts/goss.sh
+++ scripts/goss.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-GOSS_VERSION="${1:-v0.3.6}"
 set -e
 
+GOSS_VERSION="${1:-v0.3.7}"
 
 curl -L https://github.com/aelsabbahy/goss/releases/download/$GOSS_VERSION/goss-linux-amd64 -o /usr/local/bin/goss
 chmod +rx /usr/local/bin/goss
