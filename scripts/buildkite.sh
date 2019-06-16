784671048e743408290152f5cd48f44f2310c265
Andy Shinn <315485+andyshinn@users.noreply.github.com>
2019-06-15 21:51:46 -0500

    set image as buildkite tag

--- scripts/buildkite.sh
+++ scripts/buildkite.sh
@@ -4,6 +4,7 @@ set -e
 
 AGENT_TOKEN="${1:-$AGENT_TOKEN}"
 AGENT_VERSION="${2:-$AGENT_VERSION}"
+AGENT_TAGS="${3:-$AGENT_TAGS}"
 
 echo deb https://apt.buildkite.com/buildkite-agent stable main > /etc/apt/sources.list.d/buildkite-agent.list
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 32A37959C2FA5C3C99EFBC32A79206696452D198
@@ -17,4 +18,5 @@ mkdir -p /etc/systemd/system/buildkite-agent.service.d
 cat <<EOF > /etc/systemd/system/buildkite-agent.service.d/env.conf
 [Service]
 Environment=BUILDKITE_AGENT_TAGS_FROM_GCP=true
+Environment=BUILDKITE_AGENT_TAGS="$AGENT_TAGS"
 EOF
