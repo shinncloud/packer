7d918f3381d4bddb13ca01f5396000aedcc09acb
Andy Shinn <315485+andyshinn@users.noreply.github.com>
2019-06-15 21:02:10 -0500

    fix buildkite agent version

--- scripts/buildkite.sh
+++ scripts/buildkite.sh
@@ -3,7 +3,7 @@
 set -e
 
 AGENT_TOKEN="${1:-$AGENT_TOKEN}"
-AGENT_VERSION="${2:-3.8.2-2742}"
+AGENT_VERSION="${2:-$AGENT_VERSION}"
 
 echo deb https://apt.buildkite.com/buildkite-agent stable main > /etc/apt/sources.list.d/buildkite-agent.list
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 32A37959C2FA5C3C99EFBC32A79206696452D198
