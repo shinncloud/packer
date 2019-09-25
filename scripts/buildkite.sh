0ba7ecda69b8d9455f697a601c6a63262774cbcc
Andy Shinn <315485+andyshinn@users.noreply.github.com>
2019-09-24 17:47:02 -0700

    keys decrypted in packer

--- scripts/buildkite.sh
+++ scripts/buildkite.sh
@@ -1,16 +1,29 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-set -e
+set -eo pipefail
 
-AGENT_TOKEN="${1:-$AGENT_TOKEN}"
+export PATH="${PATH}:/snap/bin"
+
+AGENT_TOKEN="${1:-AGENT_TOKEN}"
 AGENT_VERSION="${2:-$AGENT_VERSION}"
 AGENT_TAGS="${3:-$AGENT_TAGS}"
+AGENT_KMS_KEYRING="${4:-AGENT_KMS_KEYRING}"
+AGENT_KMS_KEY="${4:-AGENT_KMS_KEY}"
+AGENT_KMS_LOCATION="${4:-AGENT_KMS_LOCATION}"
+
+KMS_ARGS=(
+  "--location=$AGENT_KMS_LOCATION"
+  "--keyring=$AGENT_KMS_KEYRING"
+  "--key=$AGENT_KMS_KEY"
+)
+
+BUILDKITE_KEY="$(echo -n "$AGENT_TOKEN" | base64 --decode | gcloud kms decrypt --plaintext-file=- --ciphertext-file=- "${KMS_ARGS[@]}")"
 
 echo deb https://apt.buildkite.com/buildkite-agent stable main > /etc/apt/sources.list.d/buildkite-agent.list
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 32A37959C2FA5C3C99EFBC32A79206696452D198
 apt-get update -q
 apt-get install -qy buildkite-agent="$AGENT_VERSION"
-sed -i "s/xxx/$AGENT_TOKEN/g" /etc/buildkite-agent/buildkite-agent.cfg
+sed -i "s/xxx/$BUILDKITE_KEY/g" /etc/buildkite-agent/buildkite-agent.cfg
 sed -i "s/%hostname-%n/%hostname/g" /etc/buildkite-agent/buildkite-agent.cfg
 usermod -G docker buildkite-agent
 systemctl enable buildkite-agent
@@ -22,14 +35,19 @@ Environment=BUILDKITE_AGENT_TAGS_FROM_GCP=true
 Environment=BUILDKITE_AGENT_TAGS="$AGENT_TAGS"
 EOF
 
+# Keys
 mkdir -p /var/lib/buildkite-agent/.ssh
-mv /tmp/buildkite-packer /var/lib/buildkite-agent/.ssh/buildkite-packer
-mv /tmp/buildkite-terraform /var/lib/buildkite-agent/.ssh/buildkite-terraform
+
+for key in /tmp/keys/*.enc; do
+  gcloud kms decrypt "${KMS_ARGS[@]}" \
+    --ciphertext-file="$key" \
+    --plaintext-file="/var/lib/buildkite-agent/.ssh/${key%.enc}"
+  chmod 600 "/var/lib/buildkite-agent/.ssh/${key%.enc}"
+done
+
 mv /tmp/ssh_config /var/lib/buildkite-agent/.ssh/config
 chown -R buildkite-agent:buildkite-agent /var/lib/buildkite-agent/.ssh
 chmod 700 /var/lib/buildkite-agent/.ssh
-chmod 600 /var/lib/buildkite-agent/.ssh/buildkite-packer
-chmod 600 /var/lib/buildkite-agent/.ssh/buildkite-terraform
 
 # Disable detached head warnings
 sudo -H -u buildkite-agent git config --global advice.detachedHead false
